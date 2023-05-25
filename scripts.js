e28e0e677cba1cfec07b93b76c079b0df5d9b23f
Sean <sean@Seans-Mac-mini.local>
2023-05-24 22:40:55 -0400

    update mailto form and add hr

--- scripts.js
+++ scripts.js
@@ -2,3 +2,20 @@ function openMenu() {
   var burger = document.querySelector(".burger_menu");
   burger.classList.toggle("open");
 }
+
+const form = document.querySelector(".contact-form");
+const nameInput = form.querySelector('input[name="yourName"]');
+const messageInput = form.querySelector("textarea");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const nameValue = nameInput.value;
+  const messageValue = messageInput.value;
+
+  const mailtoURL = `mailto:seanpclarey@gmail.com?subject=${encodeURIComponent(
+    nameValue
+  )}&body=${encodeURIComponent(messageValue)}%0A%0A`;
+
+  window.location.href = mailtoURL;
+});
